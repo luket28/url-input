3ad983c4bc449d102cf1b0be5489c422d1e68353
Luke Truter <luke.truter@eventyeti.com>
2015-06-23 19:37:36 +0200

    Add Defaults Setting
    Allows the user to supply default URL parts (such as protocol, sub_domain etc).

--- tests/index.php
+++ tests/index.php
@@ -1,5 +1,6 @@
 <?php
-function htmlout($text) {
+function htmlout($text)
+{
     return htmlentities(utf8_encode($text));
 }
 ?>
@@ -10,33 +11,54 @@ function htmlout($text) {
         
         <link type="text/css" href="css/template.css"  rel="stylesheet" />
         
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="js/jquery-1.11.1.min.js" type="text/javascript"><\/script>')</script>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>
+            window.jQuery || document.write(
+                '<script src="js/jquery-1.11.1.min.js" type="text/javascript"><\/script>'
+            );
+        </script>
         <script type="text/javascript" src="../src/url-input.js"></script>
         
         <script type="text/javascript">
-		$(function () {
-			$('input[type=url]').urlInput({});
-		});
+        $(function () {
+            $('input[type=url]').urlInput({
+                defaults: {
+                    'protocol': 'https'
+                }
+            });
+        });
         </script>
     </head>
     <body>
-        <form method="POST" name="social_links">
+        <form method="POST" name="social_links" novalidate>
             <h3>Social Links</h3>
             
             <div class="form-widget">
                 <label>Twitter Url:</label>
-                <input type="url" name="social_links[twitter]" class="twitter-url" value="<?php echo htmlout(@$_POST['social_links']['twitter']); ?>" />
+                <input type="url"
+                       name="social_links[twitter]"
+                       class="twitter-url"
+                       value="<?php echo htmlout(@$_POST['social_links']['twitter']); ?>"
+                    />
             </div>
             
             <div class="form-widget">
                 <label>Facebook Url:</label>
-                <input type="url" name="social_links[facebook]" class="facebook-url" value="<?php echo htmlout(@$_POST['social_links']['facebook']); ?>" />
+                <input
+                    type="url"
+                    name="social_links[facebook]"
+                    class="facebook-url"
+                    value="<?php echo htmlout(@$_POST['social_links']['facebook']); ?>"
+                    />
             </div>
             
             <div class="form-widget">
                 <label>Custom Url:</label>
-                <input type="url" name="social_links[custom]" class="globe-url" value="<?php echo htmlout(@$_POST['social_links']['custom']); ?>" />
+                <input
+                    type="url"
+                    name="social_links[custom]"
+                    class="globe-url" value="<?php echo htmlout(@$_POST['social_links']['custom']); ?>"
+                    />
             </div>
             
             <div>
